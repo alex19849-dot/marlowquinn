ad0ed97958f465d69f5c453f622b77f566a0dcfd
alex19849-dot <alex19849@gmail.com>
2026-07-01 19:04:12 +0100

    Change logo image format from JPG to PNG

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
       <nav className="mx-auto flex h-20 max-w-7xl items-center justify-between px-6">
         <Link href="/" className="transition hover:opacity-80">
           <Image
-            src="/marlow-logo.jpg"
+            src="/marlow-logo.png"
             alt="Marlow Quinn"
             width={70}
             height={70}
